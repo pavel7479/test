64af33a6b835558bacb7a1ca2db673d11a445009
pavel7479 <50517115+pavel7479@users.noreply.github.com>
2021-05-04 17:46:12 +0300

    Update Solution.java

--- task2209/Solution.java
+++ task2209/Solution.java
@@ -1,9 +1,5 @@
 package com.javarush.task.task22.task2209;
-
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.StringTokenizer;
@@ -13,13 +9,30 @@ import java.util.StringTokenizer;
 */
 
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException  {
         //...
-        StringBuilder result = getLine();
-        System.out.println(result.toString());
+        String line;
+        try (BufferedReader reader = new BufferedReader (new FileReader("C:\\a1.txt"))) {
+            line = reader.readLine();
+        }
+        String [] str = line.split(" ");
+        StringBuilder result = getLine(str);
+//        System.out.println(result.toString());
+//        for (String s : str) {
+//            System.out.println(s);
+//        }
     }
 
     public static StringBuilder getLine(String... words) {
+        String [] strings = new String[words.length];
+        for (int i = 0; i < words.length-1; i++) {
+            if (words[i].charAt(0) == words [i+1].charAt(words[i+1].length()-1)) {
+                words [i+1] = null;
+                strings [i] = words [i];
+                strings [i+1] = words [i+1];
+            }
+        }
+        StringBuilder stringBuilder =
         return null;
     }
 }
